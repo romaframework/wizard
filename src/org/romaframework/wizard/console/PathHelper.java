f5922da7490f823b230f632c599b968807164724
Tglman <tglman@tglman.com>
2011-11-09 15:04:30 +0100

    fix path helper.

--- src/org/romaframework/wizard/console/PathHelper.java
+++ src/org/romaframework/wizard/console/PathHelper.java
@@ -5,17 +5,17 @@ import java.io.IOException;
 
 public class PathHelper {
 
-	private static final String	JAVA_HOME	= "JAVA_HOME";
+	private static final String	ROMA_HOME	= "ROMA_HOME";
 
 	public static String getWizardPath() {
-		String home = System.getenv(JAVA_HOME);
+		String home = System.getenv(ROMA_HOME);
 		if (home == null || home.isEmpty()) {
 			try {
 				home = new File(".").getCanonicalPath();
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
-		} 
+		}
 		if (home.charAt(home.length() - 1) != '/') {
 			home += "/";
 		}
